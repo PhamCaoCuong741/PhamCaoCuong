847c56d4334c9edbcc9d53c133207bafff4e2fbb
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-10-05 09:34:25 +0700

    Add project files.

---
+++ PhamCaoCuong/Models/SinhViens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PhamCaoCuong.Models
+{
+        [Table("SinhVien")]
+        public partial class SinhVien
+        {
+            [Key]
+            [StringLength(20)]
+            public string MaSinhVien { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            public string HoTen { get; set; }
+
+            public int MaLop { get; set; }
+        }
+    }
